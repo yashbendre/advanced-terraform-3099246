e041e8d1115fb2c935733cee5bf870883319266d
Yash Bendre <yashdbendre@gmail.com>
2023-08-17 11:35:49 +0530

    project update

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 ### PROVIDER
 provider "google" {
   project = "advanced-396008" #replace this with your project-id
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
 }
 
 ### NETWORK
@@ -15,7 +15,7 @@ resource "google_compute_subnetwork" "subnet-1" {
   name                     = "subnet1"
   ip_cidr_range            = "10.127.0.0/20"
   network                  = data.google_compute_network.default.self_link
-  region                   = "us-central1"
+  region                   = "asia-south1"
   private_ip_google_access = true
 }
 
